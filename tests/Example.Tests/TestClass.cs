526f8a11eef4a8db808de21647eb46dd96bf35cb
Igor Chakrygin <igor@chakrygin.ru>
2021-10-10 09:56:02 +0300

    test

---
+++ tests/Example.Tests/TestClass.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Example.Tests
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethod()
+        { }
+    }
+}
